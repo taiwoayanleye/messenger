fb0635f5872b73d66216a95680c47a7cd0bb464e
t <onlyyouandty@gmail.com>
2015-07-18 17:34:30 +0100

    Created conversations controller and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :mailbox
+  helper_method :mailbox, :conversation
 
   private
 
   def mailbox
   	@mailbox ||= current_user.mailbox	
+  end
+
+  def conversation
+    @conversation ||= mailbox.conversations.find(params[:id])
   end
 
   protected
